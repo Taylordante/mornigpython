072b275ad9c4c11e67a5d0f085c355931e303e1a
Taylordante <taylordante016@gmail.com>
2026-02-03 10:27:13 +0300

    new commit

---
+++ Introduction.py
@@ -0,0 +1,21 @@
+#print is a method used to display an output
+
+
+
+
+print("Welcome to Python Programming")
+print("Mreta")
+print(100)
+print(20*3)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,27 @@
+#variable is a container used to a store value
+from pyexpat.errors import messages
+
+school="eMobilis"
+firstname="Dante"
+lastname="Mreta"
+num1=36
+num2=42
+language="Python"
+
+
+print(school)
+print(firstname,lastname)
+print(num1+num2)
+
+print(language,"is a web programming language")
+print("The sum of",num1,"and",num2,"is",num1+num2)
+
+
+
+
+
+
+
+
+
+
